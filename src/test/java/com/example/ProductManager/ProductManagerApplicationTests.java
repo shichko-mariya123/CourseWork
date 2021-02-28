2f6fa32d8c7d4151120146397bca25c8e0f54ec6
shichko-mariya123 <shichkomaria1007@gmail.com>
2021-02-28 14:42:09 +0300

    '

---
+++ src/test/java/com/example/ProductManager/ProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
